91a71aab0771c424f115c25c4205cb876ee3dc9a
henry <zs19971226@gmail.com>
2021-12-08 18:29:32 +0800

    commit

--- app/src/main/java/com/example/bikaapi/module/login/LoginActivity.java
+++ app/src/main/java/com/example/bikaapi/module/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import com.example.bikaapi.R;
 import com.example.bikaapi.databinding.ActivityLoginBinding;
+import com.gyf.immersionbar.ImmersionBar;
 
 import me.goldze.mvvmhabit.base.BaseActivity;
 import me.goldze.mvvmhabit.base.BaseViewModel;
@@ -24,6 +25,9 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding, BaseViewMo
     @Override
     public void initViewObservable() {
         super.initViewObservable();
+
+        ImmersionBar.with(this).transparentBar().init();
+
         binding.ivCenterAnim.setBackgroundResource(R.drawable.bika_loading_big);
         AnimationDrawable animationDrawable = (AnimationDrawable) binding.ivCenterAnim.getBackground();
         animationDrawable.start();
